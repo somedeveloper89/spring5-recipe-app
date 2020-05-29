5eaa2f3f079d388254267a80c9ef626a857502f0
somedeveloper89 <m.kabaktepe@outlook.com>
2020-05-29 15:27:58 +0200

    completing assignment

--- src/main/java/guru/springframework/domain/Difficulty.java
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -2,6 +2,7 @@ package guru.springframework.domain;
 
 public enum Difficulty {
     EASY,
-    MEDIUM,
+    MODERATE,
+    KIND_OF_HARD,
     HARD
 }
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,6 +17,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @Lob
@@ -27,6 +29,12 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -122,6 +130,14 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
 
